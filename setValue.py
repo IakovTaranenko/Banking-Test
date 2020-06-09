64ebccfe37dcbe71c2212ecfadeb806d45d7873e
Iakov Taranenko <48700922+IakovTaranenko@users.noreply.github.com>
2020-06-09 00:09:38 -0700

    Added Main Functions to Class

--- setValue.py
+++ setValue.py
@@ -1,27 +1,64 @@
 class setAccount():
     def setFirstName(self, givenNewName):
-        
+        if self.firstName == givenNewName:
+            return 'Cannot be the same.'
+        else:
+            self.firstName = givenNewName
+            return 'Change successful.'
     
     def setMiddleName(self, givenNewName):
-        
+        if self.middleName == givenNewName:
+            return 'Cannot be the same.'
+        else:
+            self.middleName = givenNewName
+            return 'Change successful.'
     
     def setLastName(self, givenNewName):
-        
+        if self.lastName == givenNewName:
+            return 'Cannot be the same.'
+        else:
+            self.lastName = givenNewName
+            return 'Change successful.'
     
     def setAdressLine1(self, givenNewAdress):
-        
+        if self.adressLine1 == givenNewAdress:
+            return 'Cannot be the same.'
+        else:
+            self.adressLine1 = givenNewAdress
+            return 'Change successful.'
     
     def setAdressLine2(self, givenNewAdress):
-        
+        if self.adressLine2 == givenNewAdress:
+            return 'Cannot be the same.'
+        else:
+            self.adressLine2 = givenNewAdress
+            return 'Change successful.'
     
     def setZIP(self, givenNewZIP):
-        
+        if self.ZIP == givenNewZIP:
+            return 'Cannot be the same.'
+        else:
+            self.ZIP = givenNewZIP
+            return 'Change successful.'
     
     def setCity(self, givenNewCity):
-        
+        if self.city == givenNewCity:
+            return 'Cannot be the same.'
+        else:
+            self.city = givenNewCity
+            return 'Change successful.'
     
     def setState(self, givenNewState):
-        
+        if state == givenNewState:
+            return 'Cannot be the same.'
+        else:
+            self.state = givenNewState
+            return 'Change successful.'
     
     def setCountry(self, givenNewCountry):
-        
+        if self.country == givenNewCountry:
+            return 'Cannot be the same.'
+        else:
+            self.state = givenNewCountry
+            return 'Change successful.'
+    
