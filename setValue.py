478b945c0f3d688e75219c28cd800ab1a1b39f14
Iakov Taranenko <48700922+IakovTaranenko@users.noreply.github.com>
2020-06-08 23:25:05 -0700

    New Core Files

---
+++ setValue.py
@@ -0,0 +1,27 @@
+class setAccount():
+    def setFirstName(self, givenNewName):
+        
+    
+    def setMiddleName(self, givenNewName):
+        
+    
+    def setLastName(self, givenNewName):
+        
+    
+    def setAdressLine1(self, givenNewAdress):
+        
+    
+    def setAdressLine2(self, givenNewAdress):
+        
+    
+    def setZIP(self, givenNewZIP):
+        
+    
+    def setCity(self, givenNewCity):
+        
+    
+    def setState(self, givenNewState):
+        
+    
+    def setCountry(self, givenNewCountry):
+        
